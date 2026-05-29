033e3783e2efa3e9dc449b0465202d56d73a8e56
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-29 15:26:04 +0100

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -61,6 +61,8 @@ app.delete("/users/:id", (req, res) => {
   res.json({ message: "Utilisateur supprimé" });
 });
 
-app.listen(5000, () => {
-  console.log("Serveur démarré sur http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
